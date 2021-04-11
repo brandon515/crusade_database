c70d40b1a023071d5467da0e6f01972fd5e62efe
Brandon Haley <brandon.haley94@gmail.com>
2021-04-11 08:19:04 +0000

    created the create force endpoint

--- routes/forces.js
+++ routes/forces.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 var jwt = require('jsonwebtoken');
+var config = require('../config');
 
 // Token verification middleware
-router.put('/token/:token/*', function(req, res, next) {
-    jwt.verify(req.params['token'], config.initTokenSecret, function(err, decoded) {
+router.post('/*', function(req, res, next) {
+    jwt.verify(req.body['token'], config.initTokenSecret, function(err, decoded) {
         if(err){
             console.log(err);
             res.sendStatus(401);
@@ -14,6 +15,32 @@ router.put('/token/:token/*', function(req, res, next) {
             next();
         }
     });
+});
+
+// CREATE a new force (name, player_id) combos need to be unique
+router.post('/create',
+    async function(req, res) {
+        try{
+            var factionRes = await db.query('SELECT name FROM factions WHERE faction_id=$1',
+                [req.body['faction_id']]);
+            var supplyRes = await db.query('SELECT name FROM supply_types WHERE type_id=$1',
+                [req.body['supply_type']]);
+            var supply_limit = 0;
+            if(req.body['supply_type'] === '1'){//points
+                supply_limit = 50 * 20; //each power level is equal to 20 points
+            }else if(req.body['supply_type'] === '2'){ //power level
+                supply_limit = 50; //starting power level is 50 per the rule book
+            }
+            var insertRes = await db.query('INSERT INTO forces(name,supply_limit,fluff,faction_id,player_id,supply_type) VALUES ($1,$2,$3,$4,$5,$6) RETURNING force_id',
+                [ req.body['name'], supply_limit, req.body['fluff'], req.body['faction_id'], req.token.id, req.body['supply_type'] ]); 
+            res.set({
+                'Content-Type': 'json',
+                'location' : req.baseUrl+'/'+insertRes.rows[0].force_id,
+            }).sendStatus(201);
+        }catch(err){
+            console.log(err);
+            res.sendStatus(404);
+        }
 });
 
 // READ all forces force_id, player_id, faction_id
