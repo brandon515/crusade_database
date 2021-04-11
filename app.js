0a784a490622022271a45cbef220a1b3ed9f9748
Brandon Haley <brandon.haley94@gmail.com>
2021-04-11 06:06:38 +0000

    added supply types and roles to the app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var playersRouter = require('./routes/players');
 var factionsRouter = require('./routes/factions');
+var supplyTypesRouter = require('./routes/supply_types');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/players', playersRouter);
 app.use('/factions', factionsRouter);
+app.use('/supply_types', supplyTypesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
